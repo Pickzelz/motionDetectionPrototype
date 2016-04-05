42308329c32646d50048b20a6db6a42c6b1ab118
Fandi Harsono <fandi@dexcode.com>
2016-04-05 17:51:30 +0700

    add compare image

--- assets/js/engine/CaptureImageCam.js
+++ assets/js/engine/CaptureImageCam.js
@@ -28,7 +28,6 @@
         navigator,
     		{video: true},
         function(localMediaStream){
-          console.log('masuk');
           if(webcam){
             var vendorUrl = window.URL || window.webkitURL;
 
@@ -44,10 +43,36 @@
       );
     }
 
+    function captureImage(append){
+      var canvas = document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext('2d').drawImage(webcam, 0, 0, width, height);
+
+      var pngImage = canvas.toDataURL('image/png');
+
+      if(append){
+        append.appendChild(canvas);
+      }
+
+      return canvas;
+    }
+
+    function setSize(w, h){
+      width = w;
+      height = h;
+    }
+
     function has_support(){
       return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
     }
 
     initialize(Element);
+
+    return {
+      setSize: setSize,
+      hasSupport: has_support,
+      captureImage: captureImage
+    }
   }
 })(MotionDetector)
